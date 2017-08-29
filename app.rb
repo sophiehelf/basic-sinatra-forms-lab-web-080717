2764b06b08b97c4573eebc23a8d1eed8f61f52a4
sophiehelf <sophie.helf@gmail.com>
2017-08-29 11:19:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,18 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+  get '/' do
+    "Hello World"
+  end
+
+
+	get '/newteam' do 
+		erb :newteam 
+	end
+
+	post '/team' do 
+		@params = params
+		erb :team 
+	end
+
 end
